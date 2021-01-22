f3eeaf543c97344a4978bfadf73db0fd4ff984a6
thanhlam1702 <thanhlam17021999@gmail.com>
2021-01-22 22:26:58 +0700

    file deloy

---
+++ dist/_nuxt/7225ab5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{222:function(t,e,n){"use strict";n.r(e);var l=n(23),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("About page")])}),[],!1,null,null,null);e.default=component.exports}}]);
--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="main-wrapper home">
-    <h1>Content main</h1>
-  </div>
-</template>
