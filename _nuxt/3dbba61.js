c136e0418636e28264034757e68950adb4be3acc
Nguyễn Thanh Lãm <thanhlam17021999@gmail.com>
2021-08-02 19:24:14 +0700

    fix refresh

--- _nuxt/3dbba61.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{895:function(n,t,e){"use strict";e.r(t);var l={layout:"admin"},c=e(20),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
