bcd915c9251b5b12eeaff8f95353fcb21c6fd7eb
Nguyễn Thanh Lãm <thanhlam17021999@gmail.com>
2021-08-06 22:54:18 +0700

    change struct data

---
+++ _nuxt/ad194b7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{912:function(n,t,e){"use strict";e.r(t);var l={layout:"admin"},c=e(19),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
